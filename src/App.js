2166e9b3a245f9bfcbe239bac7687a3fd2824d18
oalvarez1990 <omaraleyser@hotmail.com>
2022-03-19 13:26:15 -0500

    En react.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       Hola mundo por colombia y 22.54 
-  //     </header>
-  //   </div>
-  // );
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hola mundo por colombia y 22.54 
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componets/TodList.js
@@ -0,0 +1,34 @@
+import React, { Fragment, useState } from 'react';
+import RenderUser from './RenderUser'
+
+function Registro(props){
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [estado, setEstado] = useState([]);
+
+    function addUser(e){
+        e.preventDefault();
+        let usuario = {
+            nombre: nombre,
+            apellido: apellido
+        }
+        setEstado([...estado, usuario]);
+    }
+
+    return(
+        <Fragment>
+            <form action="" onSubmit={(e) => addUser(e)}>
+                <input onChange={(e) => setNombre(e.target.value)} type="text" id="nombre" name="nombre"/>
+                <input onChange={(e) => setApellido(e.target.value)} type="text" id="apellido" name="apellido"/>
+                <button>Registrarme</button>
+            </form>
+
+         <RenderUser user={estado}/>
+
+
+        </Fragment>
+
+    )
+}
+
+export default Registro;
