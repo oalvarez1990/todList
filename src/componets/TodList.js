953f9e593f22609ba71da88ce492fb5957c0e806
oalvarez1990 <omaraleyser@hotmail.com>
2022-03-20 22:21:24 -0500

    En react.js

--- src/componets/TodList.js
+++ src/componets/TodList.js
@@ -2,31 +2,31 @@ import React, { Fragment, useState } from 'react';
 import RenderUser from './RenderUser'
 
 function Registro(props){
-    // const [nombre, setNombre] = useState('');
-    // const [apellido, setApellido] = useState('');
-    // const [estado, setEstado] = useState([]);
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [estado, setEstado] = useState([]);
 
-    // function addUser(e){
-    //     e.preventDefault();
-    //     let usuario = {
-    //         nombre: nombre,
-    //         apellido: apellido
-    //     }
-    //     setEstado([...estado, usuario]);
-    // }
+    function addUser(e){
+        e.preventDefault();
+        let usuario = {
+            nombre: nombre,
+            apellido: apellido
+        }
+        setEstado([...estado, usuario]);
+    }
 
-    // return(
-    //     <Fragment>
-    //         <form action="" onSubmit={(e) => addUser(e)}>
-    //             <input onChange={(e) => setNombre(e.target.value)} type="text" id="nombre" name="nombre"/>
-    //             <input onChange={(e) => setApellido(e.target.value)} type="text" id="apellido" name="apellido"/>
-    //             <button>Registrarme</button>
-    //         </form>
+    return(
+        <Fragment>
+            <form action="" onSubmit={(e) => addUser(e)}>
+                <input onChange={(e) => setNombre(e.target.value)} type="text" id="nombre" name="nombre"/>
+                <input onChange={(e) => setApellido(e.target.value)} type="text" id="apellido" name="apellido"/>
+                <button>Registrarme</button>
+            </form>
 
-    //      <RenderUser user={estado}/>
+         <RenderUser user={estado}/>
 
 
-    //     </Fragment>
+        </Fragment>
 
     )
 }
